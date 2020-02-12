d81b038f324b84c886aa1b6780c423d4a8db0af4
lephones <lephones@lephones.net>
2020-02-12 19:34:34 +0800

    Initial commit

---
+++ app/src/main/java/me/lefo/jenkinstest/MainActivity.java
@@ -0,0 +1,13 @@
+package me.lefo.jenkinstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
